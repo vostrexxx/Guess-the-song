f4f55c77a9cbb8ecfad9b6325c9a0509742a8721
vostrexxx <fukksleep74@gmail.com>
2022-10-02 20:02:37 +0300

    creating frst button

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+# for access to arguments Command Line
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-import sys  # for access to arguments Command Line
+
+# child class QMainWindow for set main window of app
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Guess the song")
+        button = QPushButton("Confirm!")
+
+        # set central widget window
+        self.setCentralWidget(button)
+
 
 # transfer sys.argv to args,to access args of CL to app
-app = QApplication([])
+app = QApplication(sys.argv)
 
 # creating a widget; Qt - window
-window = QWidget()
+window = MainWindow()
 window.show()
 
-# starting event to no close
+# starting event to no close window
 app.exec()
+
